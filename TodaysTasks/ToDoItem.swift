52fe2ae0825d76d190b6c971e24c2bab4e3481ad
Jay A <justynadusei@gmail.com>
2020-03-03 23:18:09 +0000

    Buggy:(

--- TodaysTasks/ToDoItem.swift
+++ TodaysTasks/ToDoItem.swift
@@ -15,16 +15,16 @@ public class ToDoItem:NSManagedObject, Identifiable {
     @NSManaged public var title:String?
 }
 
-extension ToDoItem {
-    
-    static func getAllToDoItems() -> NSFetchRequest<ToDoItem> {
-        
-        let request:NSFetchRequest<ToDoItem> = ToDoItem.fetchRequest() as! NSFetchRequest<ToDoItem>
-        
-        let sortDescriptor = NSSortDescriptor(key: "createdAt", ascending: true)
-        
-        request.sortDescriptors = [sortDescriptor]
-        
-        return request
-    }
-}
+//extension ToDoItem {
+//
+//    static func getAllToDoItems() -> NSFetchRequest<ToDoItem> {
+//
+//        let request:NSFetchRequest<ToDoItem> = ToDoItem.fetchRequest() as! NSFetchRequest<ToDoItem>
+//
+//        let sortDescriptor = NSSortDescriptor(key: "createdAt", ascending: true)
+//
+//        request.sortDescriptors = [sortDescriptor]
+//
+//        return request
+//    }
+//}
